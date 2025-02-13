a1f42c6397829a660dd99052f9ef56fe4d82c39d
Javier Agustín Chasco <agustin.chasco@wisc.edu>
2025-02-13 15:46:03 -0600

    Add files via upload

---
+++ US24UW990EE.007_01.06_MilkProd.R
@@ -0,0 +1,12 @@
+############################### Objective ######################################
+# Same than `US24UW990EE.007_01.04_MilkProd.R`.
+# Main difference: keeping the 22% missing observations w/o milk production.
+# Replace NA by -999/ Save file.
+
+
+################################### Notes ######################################
+
+############################## Initialization ##################################
+rm(list = ls())
+
+CN <- 'US24UW990EE.007_01.05_MilkProd'
